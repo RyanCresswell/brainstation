ee541123c78e994f9cb97297ebffc6ffe862ea43
Ryan Cresswell <41250602+RyanCresswell@users.noreply.github.com>
2018-07-25 00:33:53 -0700

    Update assignment-1.js

--- assignment-1.js
+++ assignment-1.js
@@ -1,12 +1,8 @@
 var numbers = []
 for (var i=0; i < 10; i++){ // set numbers that will be included in array
     numbers.push(i); // pushes above numbers into the array
-
 console.log(numbers[0]); // logs first array number
 }
 console.log(numbers[9]); // logs last array number
-
 var car = { } //creates an object
-
-
 car.colour = "black" ; // assigns it the color black
